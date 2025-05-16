60f2a4be61ed5fdb3ea3625482e34847e6905594
Thiago Barroso <thiagoo.silva@live.com>
2025-05-16 00:31:11 -0300

    Form component e rotas do login

---
+++ src/Components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Home.module.css";
+
+function Home() {
+  return <div className={style.Home}>Home</div>;
+}
+
+export default Home;
---
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginPasswordLost() {
+  return <div>Login PasswordLost</div>;
+}
+
+export default LoginPasswordLost;
